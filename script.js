5cb2d7a5cf247fa7e82e70e61381da1d1a0beda4
LuchPuchPick <127535618+LuchPuchPick@users.noreply.github.com>
2023-08-09 17:33:01 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ $(".burger").click(function () {
     }
   });
 
+
+
+  $(".burger").click(function () {
+    if ($(".check-icon").is(":checked")) {
+      $(".logo").css("display", "none");
+    } else {
+      $(".logo").css("display", "flex");
+    }
+  });
+
+  //act-btт
+  
+    $(document).ready(function(){
+      let hiddenRow = $(".expandable");
+      $(".act-button").click(function(){
+        hiddenRow.toggleClass("hidden")
+        if (hiddenRow.is(":visible")){
+          $(".act-button").html("Свернуть")
+        }
+        else {
+          $(".act-button").html("Развернуть")
+        }
+      })
+    })
+    
